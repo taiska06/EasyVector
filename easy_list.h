b6ffc2c041b058aa542c27e3e45d2b21f6d56075
taiska06 <112109525+taiska06@users.noreply.github.com>
2023-01-29 15:00:18 +0300

    Add files via upload

---
+++ easy_list.h
@@ -0,0 +1,12 @@
+#ifndef EASY_LIST_H_INCLUDED
+#define EASY_LIST_H_INCLUDED
+#include <iostream>
+#include <vector>
+#include "easy_list.h"
+using namespace std;
+void itc_even_index_list (const vector <int> &mass, vector <int> &mass2);
+void itc_even_parts_list(const vector <int> &mass, vector <int> &mass2);
+int itc_positive_list(const vector <int> &mass);
+int itc_sl_list(const vector <int> &mass);
+bool itc_same_parts_list(const vector <int> &mass);
+#endif // EASY_LIST_H_INCLUDED
---
+++ vector.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include "easy_list.h"
+using namespace std;
+void itc_even_index_list (const vector <int> &mass, vector <int> &mass2){
+    for(int i=0; i<mass.size();i++)
+        if(i%2==0)
+            mass2.push_back(mass[i]);
+}
+void itc_even_parts_list(const vector <int> &mass, vector <int> &mass2){
+  for(int i=0; i<mass.size();i++)
+        if(i%2!=0)
+            mass2.push_back(mass[i]);
+
+}
+int itc_positive_list(const vector <int> &mass){
+    int a=0;
+    for(int i=0; i<mass.size();i++)
+        if(mass[i]>0)
+             a++;
+    return a;
+}
+int itc_sl_list(const vector <int> &mass){
+int max;
+for (int i = 0; i <mass.size(); i++) {
+        if (mass[i]>max) {
+            max=mass[i];
+        }
+        }
+  return max;
+}
+bool itc_same_parts_list(const vector <int> &mass){
+    int a=-1;
+for (int i = 0; i <mass.size(); i++) {
+        if ((mass[i]>=0 && mass[i+1]>=0) || (mass[i]<=0 && mass[i+1]<=0)) {
+            a++;
+        }
+        }
+    if(a>0)
+        return true;
+    return false;
+
+}

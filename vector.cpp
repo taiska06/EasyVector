d91f4f21aeb51e80eb07419f4258f2cb7041b9dc
taiska06 <112109525+taiska06@users.noreply.github.com>
2023-01-30 14:59:15 +0300

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -30,8 +30,8 @@ for (int i = 0; i < mass.size(); i++) {
   return max;
 }
 bool itc_same_parts_list(const vector <int> &mass){
-    int a=-1;
-for (int i = 0; i <mass.size(); i++) {
+    int a=0;
+for (int i = 0; i <mass.size()-1; i++) {
         if ((mass[i]>=0 && mass[i+1]>=0) || (mass[i]<=0 && mass[i+1]<=0)) {
             a++;
         }

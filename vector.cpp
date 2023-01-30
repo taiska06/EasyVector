68f72e3bd28d389316d51debdfdb24139bf407df
taiska06 <112109525+taiska06@users.noreply.github.com>
2023-01-30 13:24:18 +0300

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -9,7 +9,7 @@ void itc_even_index_list (const vector <int> &mass, vector <int> &mass2){
 }
 void itc_even_parts_list(const vector <int> &mass, vector <int> &mass2){
   for(int i=0; i<mass.size();i++)
-        if(i%2!=0)
+        if(mass[i]%2!=0)
             mass2.push_back(mass[i]);
 
 }

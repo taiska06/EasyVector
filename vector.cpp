bc503e471adaf89bfe3adeb615cf3a615bc6f93a
taiska06 <112109525+taiska06@users.noreply.github.com>
2023-02-05 20:57:35 +0300

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -8,18 +8,13 @@ void itc_rev_list(vector <int> &mass){
         mass[i] = mass[i] + mass[mass.size()-i-1];
         mass[mass.size()-1-i] = mass[i] - mass[mass.size()-i-1];
         mass[i] = mass[i] - mass[mass.size()-i-1];
-    }
-}
-
+    }}
  void itc_rev_par_list(vector <int> &mass){
     for(int i = 0; i < mass.size() - mass.size() % 2; i+=2){
         mass[i] = mass[i] + mass[i + 1];
         mass[i + 1] = mass[i] - mass[i + 1];
         mass[i] = mass[i] - mass[i + 1];
-    }
- }
-
-
+    } }
 void itc_rshift_list(vector <int> &mass){
     int a = mass[mass.size() - 1];
     for(int i = mass.size() - 1; i > 0; i--){
@@ -27,9 +22,6 @@ void itc_rshift_list(vector <int> &mass){
     }
     mass[0] = a;
 }
-
-
-
  void itc_super_shift_list(vector <int> &mass, int n){
     int t;
     if (n < 0){
@@ -40,8 +32,7 @@ void itc_rshift_list(vector <int> &mass){
                 mass[i] = mass[i + 1];
             }
             mass[mass.size() - 1] = t;
-        }
-    }
+        } }
     else{
         for(int b = 0; b < n; b++){
             t = mass[mass.size() - 1];
@@ -49,7 +40,5 @@ void itc_rshift_list(vector <int> &mass){
                 mass[i] = mass[i - 1];
             }
             mass[0] = t;
-        }
-    }
- }
+        }} }
 

7b65d0582774b2cad6688b0e4a15dc8d9dc8d703
taiska06 <112109525+taiska06@users.noreply.github.com>
2023-02-06 14:38:39 +0300

    Update vector0.cpp

--- vector0.cpp
+++ vector0.cpp
@@ -21,15 +21,22 @@ int itc_positive_list(const vector <int> &mass){
     return a;
 }
 int itc_sl_list(const vector <int> &mass){
-int max=0;
-for (int i = 0; i < mass.size(); i++) {
-        if (mass[i] < mass[i+1]) {
-           max++;
+     if(mass.size()==0){
+           return 0;
+    }else{
+         int max=0;
+         for (int i = 0; i < mass.size(); i++) {
+               if (mass[i] < mass[i+1]) {
+                    max++;
         }
         }
   return max;
 }
+}
 bool itc_same_parts_list(const vector <int> &mass){
+     if(mass.size()==0){
+            return 0;
+    }else{
     int a=0;
 for (int i = 0; i <mass.size(); i++) {
         if ((mass[i]>=0 && mass[i+1]>=0) || (mass[i]<0 && mass[i+1]<0)) {
@@ -39,5 +46,5 @@ for (int i = 0; i <mass.size(); i++) {
     if(a>0)
         return true;
     return false;
-
+    }
 }

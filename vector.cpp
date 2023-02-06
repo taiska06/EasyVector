6b659680413ac155b710131391eaa693a94f7df8
taiska06 <112109525+taiska06@users.noreply.github.com>
2023-02-06 21:00:58 +0300

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -26,4 +26,15 @@ void itc_rev_list(vector <int> &mass){
     }
     }
  }
+void itc_rshift_list(vector <int> &mass){
+     if(mass.size()==0){
+    }else{
+        int a = mass[mass.size()-1];
+        for(int i=mass.size()-1; i>0;i--){
+            mass[i] = mass[i-1];
+        }
+        mass[0] = a;
+
+}
+}
 
